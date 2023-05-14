170551b7dbe0fee2d12b075b8e30b1b8a6220f00
Manikanta M <manikantapoojary444@gmail.com>
2023-05-14 15:00:17 +0530

    added transaction manager

--- Spring-transaction-01/src/main/java/com/practice/transaction/config/ProjectConfig.java
+++ Spring-transaction-01/src/main/java/com/practice/transaction/config/ProjectConfig.java
@@ -4,14 +4,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.TransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan("com")
+@EnableTransactionManagement
 public class ProjectConfig {
 
+    @Bean
     public DataSource dataSource(){
         return new DriverManagerDataSource("url"
                 ,"username",
@@ -21,6 +26,11 @@ public class ProjectConfig {
     @Bean
     public JdbcTemplate jdbcTemplate(){
         return new JdbcTemplate(dataSource());
+    }
+
+    @Bean
+    public TransactionManager transactionManager(DataSource dataSource){
+        return new DataSourceTransactionManager(dataSource);
     }
 
 
--- Spring-transaction-01/src/main/java/com/practice/transaction/service/ProductService.java
+++ Spring-transaction-01/src/main/java/com/practice/transaction/service/ProductService.java
@@ -4,6 +4,7 @@ import com.practice.transaction.dao.ProductDao;
 import com.practice.transaction.dto.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ProductService {
@@ -11,12 +12,16 @@ public class ProductService {
     @Autowired
     private ProductDao  productDao;
 
+    @Transactional
    public void saveProductInfo(){
         Product product=new Product();
         for(int i=1;i<=10;i++){
             product.setId(i);
             product.setName("testing product "+i);
             productDao.saveProduct(product);
+            if(i==8){
+                throw  new RuntimeException("error occcured");
+            }
         }
     }
 }
